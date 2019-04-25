3cc0e4d8e883b2573c8a52203c84564e01f21544
caibirdme <englanq@126.com>
2019-04-25 21:51:05 +0800

    424

--- src/lib.rs
+++ src/lib.rs
@@ -94,6 +94,7 @@ mod prob_410;
 mod prob_413;
 mod prob_417;
 mod prob_421;
+mod prob_424;
 mod prob_1002;
 mod prob_1003;
 
---
+++ src/prob_424.rs
@@ -0,0 +1,73 @@
+use std::cmp::{max, min};
+macro_rules! pos {
+    ($e:expr) => {
+        (($e-b'A') as usize)
+    };
+}
+
+impl Solution {
+    pub fn character_replacement(s: String, k: i32) -> i32 {
+        let n = s.len();
+        let k = k as usize;
+        if k+1 >= n {
+            return n as i32;
+        }
+        let none = n+10;
+        let mut prec = [none; 26];
+        let mut pre_idx = vec![none; n];
+        let mut ans = k+1;
+        let bytes = s.as_bytes();
+        prec[pos!(bytes[0])] = 0;
+        let mut last = 1;
+        for i in 1..n {
+            let c = bytes[i];
+            let t = pos!(c);
+            if prec[t] != none {
+                pre_idx[i] = prec[t];
+            }
+            prec[t] = i;
+            if i<=k {
+                last = i+1;
+                continue;
+            }
+            if c == bytes[i-1] {
+                last += 1;
+                ans = max(ans, last);
+                continue;
+            }
+            let mut cur = i;
+            let mut rest = k;
+            while pre_idx[cur] != none && rest >= 0 {
+                let replaced = cur - pre_idx[cur]-1;
+                if replaced > rest {
+                    break;
+                }
+                cur = pre_idx[cur];
+                rest -= replaced;
+            }
+            last = i-cur+1+rest;
+            ans = max(ans, min(last,i+1));
+        }
+        ans as i32
+    }
+}
+
+struct Solution;
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    #[test]
+    fn test_character_replacement() {
+        let test_cases = vec![
+            ("AAAA", 2, 4),
+            ("AABABBA", 1, 4),
+            ("ABAB", 2, 4),
+            ("AACDBAB", 3, 6),
+            ("AACDBBA", 3, 5),
+        ];
+        for (s, k, expect) in test_cases {
+            assert_eq!(expect, Solution::character_replacement(s.clone().to_string(), k), "s: {}, k:{}", s, k);
+        }
+    }
+}
