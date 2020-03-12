e25f4354a9110c11f5eb1bef9d70b5a09daa8fa0
caibirdme <englanq@126.com>
2020-03-12 23:02:44 +0800

    255

--- src/prob_255.rs
+++ src/prob_255.rs
@@ -3,48 +3,23 @@ impl Solution {
         if preorder.len() <= 1 {
             return true;
         }
-        let mut idx = 1;
-        Self::dfs(&mut idx, &preorder, None, Some(preorder[0]-1));
-        Self::dfs(&mut idx, &preorder, Some(preorder[0]+1), None);
-        idx == preorder.len()
-    }
-    fn dfs(i: &mut usize, v: &Vec<i32>, l: Option<i32>, r: Option<i32>) {
-        if *i >= v.len() {
-            return;
+        let mut q = vec![];
+        let mut lower_bound = std::i32::MIN;
+        for v in preorder{
+            if v < lower_bound {
+                return false;
+            }
+            while let Some(&top) = q.last() {
+                if v > top {
+                    q.pop();
+                    lower_bound = top;
+                } else {
+                    break;
+                }
+            }
+            q.push(v);
         }
-        let c = v[*i];
-        match (l,r) {
-            (None, Some(r_bound)) => {
-                if c <= r_bound {
-                    *i += 1;
-                    Self::dfs(i, v, None, Some(c-1));
-                    if c+1 <= r_bound {
-                        Self::dfs(i,v,Some(c+1), r);
-                    }
-                }
-            },
-            (Some(l_bound), None) => {
-                if c >= l_bound {
-                    *i += 1;
-                    if c-1 >= l_bound {
-                        Self::dfs(i, v, l, Some(c-1));
-                    }
-                    Self::dfs(i,v,Some(c+1), None);
-                }
-            },
-            (Some(l_bound), Some(r_bound)) => {
-                if c >= l_bound && c <= r_bound {
-                    *i += 1;
-                    if c-1 >= l_bound {
-                        Self::dfs(i,v,l,Some(c-1));
-                    }
-                    if c+1 <= r_bound {
-                        Self::dfs(i,v,Some(c+1), r);
-                    }
-                }
-            },
-            _ => return,
-        }
+        true
     }
 }
 
