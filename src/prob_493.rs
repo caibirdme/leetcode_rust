d6b3568d1c7441c5fdabed796010165b30906b4c
caibirdme <englanq@126.com>
2020-03-16 21:36:23 +0800

    493 bit

--- src/prob_493.rs
+++ src/prob_493.rs
@@ -1,4 +1,4 @@
-use std::cmp::Ordering;
+use std::collections::{BTreeSet, HashMap};
 
 impl Solution {
     pub fn reverse_pairs(mut nums: Vec<i32>) -> i32 {
@@ -7,55 +7,83 @@ impl Solution {
             return 0;
         }
         let mut ans = 0;
-        Self::merge_sort(&mut nums, 0, n-1, &mut ans);
+        let mut tmp = vec![];
+        Self::merge_sort(&mut nums, 0, n-1, &mut tmp, &mut ans);
         ans as i32
     }
-    fn merge_sort(nums: &mut Vec<i32>, l: usize, r: usize, ans: &mut usize) {
-        static BOUND: i32 = (std::i32::MAX-1)/2;
+    fn merge_sort(nums: &mut Vec<i32>, l: usize, r: usize, tmp: &mut Vec<i32>, ans: &mut usize) {
         if l >= r { return ; }
         let mid = (l+r)/2;
-        Self::merge_sort(nums, l, mid, ans);
-        Self::merge_sort(nums, mid+1, r, ans);
-        let n = r-l+1;
-        let mut tmp = Vec::with_capacity(n);
+        Self::merge_sort(nums, l, mid, tmp, ans);
+        Self::merge_sort(nums, mid+1, r, tmp, ans);
+        let mut k = l;
+        for i in mid+1..=r {
+            if let Some(double) = nums[i].checked_mul(2) {
+                while k <= mid && nums[k] <= double { k+=1;}
+                *ans += mid+1-k;
+            } else if nums[i] < 0 { // < i32::MIN overflow
+                *ans += mid+1-k;
+            } else { // > i32::MAX overflow
+                break;
+            }
+        }
         let mut i = l;
         let mut j = mid+1;
+        tmp.clear();
         while i <= mid && j <= r {
             if nums[i] < nums[j] {
-                if nums[j] < 0 {
-                    let q = nums[i]/2-1;
-                    if let Err(pos) = nums[j..r+1].binary_search_by(|&probe| {
-                        if probe <= q {
-                            Ordering::Less
-                        } else {
-                            Ordering::Greater
-                        }
-                    }) {
-                        *ans += pos;
-                    }
-                }
                 tmp.push(nums[i]);
                 i+=1;
             } else {
-                let q = nums[j] as i64 * 2+1;
-                if let Err(pos) = nums[i..mid+1].binary_search_by(|&probe| {
-                    if (probe as i64) < q {
-                        Ordering::Less
-                    } else {
-                        Ordering::Greater
-                    }
-                }) {
-                    *ans += mid+1-i-pos;
-                }
                 tmp.push(nums[j]);
                 j+=1;
             }
         }
         while i <= mid {tmp.push(nums[i]); i+=1;}
         while j <= r {tmp.push(nums[j]); j+=1;}
-        for i in 0..n {
+        for i in 0..(r-l+1) {
             nums[l+i] = tmp[i];
         }
+    }
+    pub fn reverse_pairs_bit(nums: Vec<i32>) -> i32 {
+        if nums.len() < 2 {
+            return 0;
+        }
+        let mut set = BTreeSet::new();
+        for &v in &nums {
+            set.insert(v as i64);
+            set.insert(v as i64*2);
+        }
+        let n = set.len();
+        let mut hash = HashMap::new();
+        for (idx, &v) in set.iter().enumerate() {
+            hash.insert(v, idx+1);
+        }
+        let mut ans = 0;
+        let mut bits = vec![0; n+1];
+        let mut total = 0;
+        for &v in nums.iter() {
+            ans += total - Self::query(&bits, *hash.get(&(v as i64*2)).unwrap());
+            Self::update(&mut bits, *hash.get(&(v as i64)).unwrap());
+            total += 1;
+        }
+        ans
+    }
+    fn update(bits: &mut Vec<i32>, idx: usize) {
+        let mut idx = idx as i32;
+        while idx < bits.len() as i32 {
+            bits[idx as usize] += 1;
+            idx += idx & -idx;
+        }
+    }
+    fn query(bits: &Vec<i32>, idx: usize) -> i32 {
+        let mut idx = idx as i32;
+        let mut ans = 0;
+        while idx > 0 {
+            ans += bits[idx as usize];
+            idx -= idx&-idx;
+        }
+        ans
     }
 }
 
@@ -63,20 +91,7 @@ struct Solution;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    #[test]
-    fn test_binary_search() {
-        let v = vec![1,2,3,3,3];
-        let t = v.binary_search_by(|&probe| {
-            if (probe as i64) < 3 {
-                Ordering::Less
-            } else {
-                Ordering::Greater
-            }
-        });
-        assert_eq!(t, Err(2));
-    }
+    use super::Solution;
 
     #[test]
     fn test_reverse_pairs() {
@@ -85,7 +100,7 @@ mod tests {
             (vec![233,2000000001,234,2000000006,235,2000000003,236,2000000007,237,2000000002,2000000005,233,233,233,233,233,2000000004], 40),
         ];
         for (nums, expect) in test_cases {
-            assert_eq!(Solution::reverse_pairs(nums.clone()), expect, "nums: {:?}", nums);
+            assert_eq!(Solution::reverse_pairs_bit(nums.clone()), Solution::reverse_pairs(nums.clone()),"nums: {:?}", nums);
         }
     }
 }
